542f8806085e7ba8c543d924f457231743e4449d
cameron zwane <cameron.cohen@bbd.co.za>
2024-03-05 11:53:33 +0200

    feature(solution): Added spring initialized project.

---
+++ src/test/java/com/beantastic/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beantastic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
