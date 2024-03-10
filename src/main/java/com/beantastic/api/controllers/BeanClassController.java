f9ced66d81ba224e0f3a53c426318c77d2384dc4
Liam Talberg <liamtalberg@gmail.com>
2024-03-10 12:05:58 +0200

    Added response codes

--- src/main/java/com/beantastic/api/controllers/BeanClassController.java
+++ src/main/java/com/beantastic/api/controllers/BeanClassController.java
@@ -6,6 +6,9 @@ import java.util.stream.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import com.beantastic.api.dao.BeanClassRepository;
 import com.beantastic.api.models.dto.BeanClassDTO;
 
@@ -20,13 +23,17 @@ public class BeanClassController {
     }
 
     @GetMapping("/beanClasses")
-    public Iterable<BeanClassDTO> getAllBeanClasses() {
+    public ResponseEntity<?> getAllBeanClasses() {
 
-        List<BeanClassDTO> beanDTOClasses = StreamSupport.stream(beanClassRepository.findAll().spliterator(), false)
-                .map(BeanClassDTO::new)
-                .collect(Collectors.toList());
+        try {
+            List<BeanClassDTO> beanDTOClasses = StreamSupport.stream(beanClassRepository.findAll().spliterator(), false)
+                    .map(BeanClassDTO::new)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(beanDTOClasses);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: " + e.getMessage());
 
-        return beanDTOClasses;
+        }
+
     }
-
 }
--- src/main/java/com/beantastic/api/models/dto/BeanClassDTO.java
+++ src/main/java/com/beantastic/api/models/dto/BeanClassDTO.java
@@ -5,15 +5,20 @@ import java.util.*;
 import com.beantastic.api.models.entities.BeanClass;
 import com.beantastic.api.models.entities.BeanClassStatistic;
 import com.beantastic.api.models.entities.StatisticType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class BeanClassDTO {
+    @JsonProperty("name")
     private String name;
 
+    @JsonProperty("description")
     private String description;
 
+    @JsonProperty("beanClassStatistics")
     private List<Map<String, Integer>> beanClassStatistics;
 
     public BeanClassDTO(BeanClass beanClass) {
+
         List<Map<String, Integer>> statsList = new ArrayList<>();
         for (BeanClassStatistic entry : beanClass.getBeanClassStatistics()) {
 
