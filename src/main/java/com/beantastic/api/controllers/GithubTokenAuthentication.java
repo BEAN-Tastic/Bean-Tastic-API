3d704dad78e2b1074a87d5151b939eb0aa6088ff
cameron zwane <cameron.cohen@bbd.co.za>
2024-03-14 19:08:32 +0200

    fixed authorization

--- src/main/java/com/beantastic/api/controllers/GithubTokenAuthentication.java
+++ src/main/java/com/beantastic/api/controllers/GithubTokenAuthentication.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -35,9 +36,17 @@ public class GithubTokenAuthentication extends OncePerRequestFilter {
         return null;
     }
 
-    private boolean isValidToken(String token) {
-        // Add your token validation logic here, such as validating against GitHub's authentication service
-        // For demonstration purposes, we assume all non-empty tokens are valid
-        return StringUtils.hasText(token);
+    private boolean isValidToken(String token){
+        try {
+            String response = RestClient.create().get()
+                    .uri("https://api.github.com/user")
+                    .header("Authorization", "Bearer " + token)
+                    .retrieve()
+                    .body(String.class);
+            if (response == null || response.trim().isEmpty()) return false;
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
